6c0f3b26dc4e5041f7ade500aace0d665fd3d668
Adil Rahman <arahman.99@outlook.com>
2023-08-17 14:13:12 +0100

    refactored to connect and query from db without tests

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,12 @@
 import express from "express";
 import cors from "cors";
+import { Client } from "pg";
 import dotenv from "dotenv";
-import {
-  addDummyDbItems,
-  addDbItem,
-  getAllDbItems,
-  getDbItemById,
-  DbItem,
-  updateDbItemById,
-  deleteDbItemById,
-} from "./db";
 import filePath from "./filePath";
 
-// loading in some dummy items into the database
-// (comment out if desired, or change the number)
+const client = new Client({ database: "todoDb" });
+
+client.connect();
 
 const app = express();
 
@@ -25,8 +18,8 @@ app.use(cors());
 // read in contents of any environment variables in the .env file
 dotenv.config();
 
-// use the environment variable PORT, or 5050 as a fallback
-const PORT_NUMBER = process.env.PORT ?? 5050;
+// use the environment variable PORT, or 4000 as a fallback
+const PORT_NUMBER = process.env.PORT ?? 4000;
 
 // API info page
 app.get("/", (req, res) => {
@@ -35,47 +28,80 @@ app.get("/", (req, res) => {
 });
 
 // GET /items
-app.get("/items", (req, res) => {
-  const allTodos = getAllDbItems();
-  res.status(200).json(allTodos);
-});
-
-// POST /items
-app.post<{}, {}, DbItem>("/items", (req, res) => {
-  // to be rigorous, ought to handle non-conforming request bodies
-  // ... but omitting this as a simplification
-  const postData = req.body;
-  const createdTodo = addDbItem(postData);
-  res.status(201).json(createdTodo);
+app.get("/items", async (req, res) => {
+  const allTodos = await client.query("select * from todos");
+  res.status(200).json(allTodos.rows);
 });
 
 // GET /items/:id
-app.get<{ id: string }>("/items/:id", (req, res) => {
-  const matchingTodo = getDbItemById(parseInt(req.params.id));
-  if (matchingTodo === "not found") {
-    res.status(404).json(matchingTodo);
+app.get("/items/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const matchingTodo = await client.query(
+    "select * from todos where id = ($1)",
+    [id]
+  );
+  if (id === undefined) {
+    res
+      .status(404)
+      .json({ status: "fail", message: "Could not find todo with that id" });
   } else {
-    res.status(200).json(matchingTodo);
+    res.status(200).json(matchingTodo.rows);
+  }
+});
+
+// POST /items
+app.post("/items", async (req, res) => {
+  const { description } = req.body;
+  const createdTodo = await client.query(
+    "insert into todos (description) values ($1) returning *",
+    [description]
+  );
+  res.status(201).json(createdTodo.rows);
+});
+
+// PATCH /items/:id
+app.patch("/items/:id", async (req, res) => {
+  const id = req.params.id;
+  const { description, status } = req.body;
+  console.log("description: ", description, " status: ", status);
+  const result = await client.query(
+    "update todos set description = ($1), status = ($2) where id = ($3) returning *",
+    [description, status, id]
+  );
+  if (result.rowCount === 1) {
+    res.status(200).json({
+      status: "success",
+      updatedTodo: result.rows,
+    });
+  } else {
+    res.status(404).json({
+      status: "fail",
+      data: {
+        id: "Could not find a todo with that id identifier",
+      },
+    });
   }
 });
 
 // DELETE /items/:id
-app.delete<{ id: string }>("/items/:id", (req, res) => {
-  const matchingTodo = deleteDbItemById(parseInt(req.params.id));
-  if (matchingTodo === "not found") {
-    res.status(404).json(matchingTodo);
-  } else {
-    res.status(200).json(matchingTodo);
-  }
-});
+app.delete("/items/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const result = await client.query("delete from todos where id = ($1)", [id]);
 
-// PATCH /items/:id
-app.patch<{ id: string }, {}, Partial<DbItem>>("/items/:id", (req, res) => {
-  const matchingTodo = updateDbItemById(parseInt(req.params.id), req.body);
-  if (matchingTodo === "not found") {
-    res.status(404).json(matchingTodo);
+  const didRemove = result.rowCount === 1;
+
+  if (didRemove) {
+    console.log(`deleted the todo with id: ${id}`);
+    res.status(200).json({
+      status: "success",
+      message: `Deleted signature with id ${id}`,
+    });
   } else {
-    res.status(200).json(matchingTodo);
+    console.log(`could not delete the todo with id: ${id}`);
+    res.status(404).json({
+      status: "fail",
+      message: "Could not find a signature with that id identifier",
+    });
   }
 });
 
