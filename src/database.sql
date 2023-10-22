52654e80c9d7c1c14835f7f401b5b1d3bd4ad7b8
Adil Rahman <82183381+adil-rahman1@users.noreply.github.com>
2023-10-22 09:39:06 +0100

    updated CREATE TABLE command

--- src/database.sql
+++ src/database.sql
@@ -1,7 +1,7 @@
 CREATE TABLE todos (
     id serial primary key,
     description text not null,
-    status text default 'pending',
+    status text not null default 'pending' check (status in ('pending','completed')),
     creationDate timestamp not null default now()
     dueDate timestamp not null
 );
