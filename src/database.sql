c3714a05bfb11739c5fbb7c6ec82ffa175c315c1
Adil Rahman <82183381+adil-rahman1@users.noreply.github.com>
2023-10-26 00:26:06 +0100

    minor update to database.sql

--- src/database.sql
+++ src/database.sql
@@ -2,14 +2,6 @@ CREATE TABLE todos (
     id serial primary key,
     description text not null,
     status text not null default 'pending' check (status in ('pending','completed')),
-    creationDate timestamp not null default now()
+    creationDate timestamp not null default now(),
     dueDate timestamp not null
 );
-
-
-
-
-
-
-
-
