70939964a8e3566c594e8abd8797c196d44aa2d0
Adil Rahman <82183381+adil-rahman1@users.noreply.github.com>
2023-10-28 19:03:11 +0100

    created columns with quotes for camelcase

--- src/database.sql
+++ src/database.sql
@@ -2,6 +2,6 @@ CREATE TABLE todos (
     id SERIAL PRIMARY KEY,
     description TEXT NOT NULL,
     status TEXT NOT NULL DEFAULT 'pending' CHECK (STATUS IN ('pending','completed')),
-    creationDate DATE NOT NULL DEFAULT CURRENT_DATE,
-    dueDate DATE NOT NULL
+    "creationDate" DATE NOT NULL DEFAULT CURRENT_DATE,
+    "dueDate" DATE NOT NULL
 );
